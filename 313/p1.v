68a5262f757ac657b66751598f369902def07fe9
Jonker <mjonker@cocsce-l3d22-10.ds.sc.edu>
2023-02-03 13:26:26 -0500

    added 313 src backup:

---
+++ 313/p1.v
@@ -0,0 +1,22 @@
+// 2023 Hunter Frady and Mitchell Jonker
+module p1 (
+	// inputs of the top-level-design
+	input CLOCK_50,	// clock
+	input [1:0] KEY,	// reset was [3:0]
+	input [1:0] SW,
+	output [17:0] LEDR,
+	output [7:0] LEDG
+	
+);
+
+	nios_system u0 (
+		.clk_clk	(CLOCK_50),	// clk.clk
+		.reset_reset_n	(KEY[0]),	// reset.reset_n
+		.cout_export (LEDG),		// counter.export
+		.modes_export (SW[1:0]),	// modes.export
+		.pattern_export (LEDR)		// pattern.export
+	);
+	
+	
+	endmodule
+	
